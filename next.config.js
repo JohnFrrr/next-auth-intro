7b2389d48916f2eeb56c4197ca5eab2a49e978e2
João Ferreira <joaov.ssff@gmail.com>
2023-07-02 23:25:41 -0300

    update 6

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    output: 'standalone',
+    compress: false,
     images: {
+        unoptimized: true,
         remotePatterns: [
             {
                 protocol: 'https',
